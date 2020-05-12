6c6425934c6aa9e35d811a1c28bea226d306f0bd
cbw29512 <divclass01@gmail.com>
2020-05-12 16:47:50 -0400

    working

--- bigcow/views.py
+++ bigcow/views.py
@@ -1,23 +1,40 @@
-from django.shortcuts import render, reverse, HttpResponseRedirect
-from bigcow.models import CowInput
+from django.shortcuts import render
 from bigcow.forms import AddTextInput
+from bigcow.models import CowInput
 import subprocess
-# Create your views here.
-
-# def index(request):
-#     data = CowInput.objects.all()
-#     return render(request, 'index.html', {'data': data})
 
 
 def index(request):
-    cow_data = CowInput.objects.all()
-    form = AddTextInput()
     if request.method == "POST":
         form = AddTextInput(request.POST)
         if form.is_valid():
-            data = form.cleaned_data
-            CowInput.objects.create(
-                cow_input=data['cow_input']
-            )            
-        return HttpResponseRedirect(reverse('homepage'))
-    return render(request, 'index.html', {'cow_data': cow_data, 'form': form})
+            cow_data = form.cleaned_data
+            CowInput.objects.create(cow_input=cow_data['cow_input'])
+            cowput = cow_words(cow_data['cow_input'])
+            form = AddTextInput()
+            return render(
+                request, 'index.html',
+                {"form": form,  'cowput': cowput}
+                )
+        form = AddTextInput()
+    return render(request, 'index.html', {'form': AddTextInput()})
+
+
+def cow_words(string):
+    cow_output = subprocess.Popen(
+        ['cowsay', string],
+        stdout=subprocess.PIPE).communicate()[0]
+    return cow_output.decode("utf8")
+
+
+def history(request):
+    cow_data = CowInput.objects.all()
+    cow_history = []
+    for cow in cow_data:
+        cow_history.append(cow)
+    cow_history = cow_history[-10:]
+    cow_history.reverse()
+    cow = cow_words('Our Last 10 Cows said:')
+    return render(request, 'history.html', {
+        "cow_history": cow_history, "cow": cow
+        })
